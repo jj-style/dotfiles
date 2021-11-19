118cfe9694e3216b88621b905ced2da97362aed9
JJ Style <style.jj@pm.me>
2021-11-19 10:12:26 +0000

    run qt on x alias

--- bash/.bash.d/environment.bash
+++ bash/.bash.d/environment.bash
@@ -2,3 +2,6 @@ export WWW_HOME=https://html.duckduckgo.com/html/
 export EDITOR=vim
 export PAGER=less
 export FZF_DEFAULT_COMMAND='rg --files'
+
+# run QT applications on xorg-wayland
+alias qt-on-x="export QT_QPA_PLATFORM=xcb"
