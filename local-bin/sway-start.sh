846d518166fee2fb28c6104b7adf620cba2804b3
JJ Style <style.jj@pm.me>
2022-07-18 17:33:22 +0100

    remove env vars from sway-start

--- local-bin/sway-start.sh
+++ local-bin/sway-start.sh
@@ -3,8 +3,4 @@
 eval $(/usr/bin/gnome-keyring-daemon --start --components=secrets)
 export SSH_AUTH_SOCK
 
-# other session wide environment variables set here
-export LIBGL_ALWAYS_SOFTWARE=1
-export MOZ_ENABLE_WAYLAND=1
-
 /sbin/sway --unsupported-gpu
