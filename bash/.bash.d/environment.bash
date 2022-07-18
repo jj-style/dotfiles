218fe7caba1c8c9257e5bbaf5bdfd32584a4d7fc
JJ Style <style.jj@pm.me>
2022-07-18 17:13:42 +0100

    update bash environment variables

--- bash/.bash.d/environment.bash
+++ bash/.bash.d/environment.bash
@@ -2,3 +2,5 @@ export WWW_HOME=https://html.duckduckgo.com/html/
 export EDITOR=vim
 export PAGER=less
 export FZF_DEFAULT_COMMAND='rg --files'
+export XONSH_COLOR_STYLE="fruity"
+export BARTIB_FILE=~/Sync/bartib.txt
